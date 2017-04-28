1f6f828c06989aad9e92bcc0ba51266324a3984b
Matthew McDade <Matthew.McDade13@gmail.com>
2017-04-28 16:02:08 -0700

    Created Home page to upload files and a controller to read file and send back its size in bytes

---
+++ FileMetadataMicroservice/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMetadataMicroservice.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpPost]
+        public IActionResult GetFileSize(IFormFile file)
+        {
+            if (file == null)
+            {
+                return BadRequest();
+            }
+
+            return Json(new { Size = file.Length });
+        }
+    }
+}
