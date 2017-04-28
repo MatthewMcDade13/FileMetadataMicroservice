1871830c2805e8e77df3f915469cf3dc4eb9ba7d
Matthew McDade <Matthew.McDade13@gmail.com>
2017-04-28 14:34:41 -0700

    Set up routes and JSON options

--- FileMetadataMicroservice/Startup.cs
+++ FileMetadataMicroservice/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace FileMetadataMicroservice
 {
@@ -16,6 +17,15 @@ namespace FileMetadataMicroservice
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddMvc()
+                .AddJsonOptions( options =>
+                {
+                    options.
+                    SerializerSettings
+                    .ContractResolver =
+                    new CamelCasePropertyNamesContractResolver();
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,10 +37,19 @@ namespace FileMetadataMicroservice
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc( routes =>
+            {
+                routes.MapRoute(
+                    "Default",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "Home", action = "Index" });                
             });
         }
     }
